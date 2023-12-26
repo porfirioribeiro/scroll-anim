7a449ab936d43f6ed93c29665f46ecedfd347e07
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2023-12-26 20:45:56 +0000

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  extends: [
+    "prettier/prettier",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export function createScrollAnimation() {
+    console.log('createScrollAnimation');
+    
+}
