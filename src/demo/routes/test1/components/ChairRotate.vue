349bc156cb03dca35302da02f17932c94d46b04b
Porfirio Ribeiro <porfirioribeiro@gmail.com>
2024-01-05 23:31:42 +0000

    feat: show text after finished

--- src/demo/routes/test1/components/ChairRotate.vue
+++ src/demo/routes/test1/components/ChairRotate.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { createScrollViewTimeline } from '$lib';
+import { createScrollViewTimeline, defineEnterAnimation } from '$lib';
 import { ref, watch } from 'vue';
 
-const subject = ref<HTMLElement | null>(null);
+const subjectRef = ref<HTMLElement | null>(null);
+const finishedRef = ref<HTMLElement | null>(null);
 
 const length = 57;
 const start = 120 - length;
@@ -11,14 +12,14 @@ const images = Array.from(
   (_, i) => `https://s3-us-west-2.amazonaws.com/s.cdpn.io/68939/360-${String(start + i + 1).padStart(3, '0')}.jpg`,
 );
 
-watch(subject, (el) => {
-  if (!el) return;
-  const imgs = el.querySelectorAll('img');
+watch([subjectRef, finishedRef], ([subject, finished]) => {
+  if (!subject) return;
+  const imgs = subject.querySelectorAll('img');
 
   let currentImg = imgs[0];
 
   createScrollViewTimeline({
-    subject: el,
+    subject,
     rangeStart: 'contain 0%',
     rangeEnd: 'contain 50%',
     onScroll(value) {
@@ -31,19 +32,35 @@ watch(subject, (el) => {
       }
     },
   });
+
+  if (!finished) return;
+
+  defineEnterAnimation({
+    target: finished,
+    subject,
+    keyframes: [{ opacity: 0 }, { opacity: 1 }],
+    rangeStart: 'contain 50%',
+    rangeEnd: 'contain 50%',
+  });
 });
 </script>
 
 <template>
-  <div ref="subject" class="chair-rotate">
+  <div ref="subjectRef" class="chair-rotate">
     <img v-for="(image, i) in images" :key="i" :src="image" />
+    <p ref="finishedRef">Finished</p>
   </div>
 </template>
 
 <style scoped>
 .chair-rotate {
-  /* display: none; */
+  position: relative;
 }
+.chair-rotate p {
+  position: absolute;
+  inset: 30%;
+}
+
 .chair-rotate img {
   display: none;
 }
